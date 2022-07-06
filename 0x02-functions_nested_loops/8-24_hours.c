a0a3cecff6abf1c9718912960b275f7371eb87a5
mihiret40 <mihiret4027@gmail.com>
2022-07-06 07:12:49 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,42 +2,62 @@
 
 /**
  *
- *  * jack_bauer - prints time table in this format: HH:MM
+ *  * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
  *
- *   * Return: the sum of two numbers.
+ *   * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  *
- *    */
+ *    * and resets mins
+ *
+ *     * Return: 0
+ *
+ *      */
 
 void jack_bauer(void)
 
 {
 
-		int h, m;
+		int hours = 0;
+
+			int minutes = 0;
+
+				int hours_remainder;
+
+					int mins_remainder;
 
 
 
-			for (h = 0; h < 24; h++)
+						while (hours <= 23)
 
-					{
+								{
 
-								for (m = 0; m < 60; m++)
+										while (minutes <= 59)
 
-											{
+												{
 
-															_putchar((h / 10) + '0');
+														mins_remainder = minutes % 10;
 
-																		_putchar((h % 10) + '0');
+															hours_remainder = hours % 10;
 
-																					_putchar(':');
+																_putchar(hours / 10 + '0');
 
-																								_putchar((m / 10) + '0');
+																	_putchar(hours_remainder + '0');
 
-																											_putchar((m % 10) + '0');
+																		_putchar(':');
 
-																														_putchar('\n');
+																			_putchar(minutes / 10 + '0');
 
-																																}
+																				_putchar(mins_remainder + '0');
 
-									}
+																					minutes++;
+
+																						_putchar('\n');
+
+																							}
+
+											hours++;
+
+												minutes = 0;
+
+								}
 
 }
