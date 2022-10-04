922f1c0a4db331b80bbca497e6e0655ccdb9092b
mihiret40 <mihiret4027@gmail.com>
2022-10-04 01:20:01 -0700

    dynamics libraries

---
+++ 0x18-dynamic_libraries/tests/0-main.c
@@ -0,0 +1,25 @@
+#include "../main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - check the code for Alx School students.
+ *
+ *   *
+ *
+ *    * Return: Always EXIT_SUCCESS.
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("%d\n", _strlen("Main"));
+
+			return (EXIT_SUCCESS);
+
+}
